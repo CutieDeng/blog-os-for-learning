31acc8a04ccf4099d8fa65675b9ed3201e437679
Cutie Deng <Dengzr2020.mail.sustech.edu.cn>
2022-06-07 22:10:27 +0800

    Add the println-macro support in VGA mode.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,31 @@
 
 // use core::panic::PanicInfo; 
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
     // todo!();  
+    println!("{}", info); 
     loop {} 
 }
 
+
+mod vga_buffer; 
+
+// use crate::vga_buffer::Writer; 
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop{} 
+
+    // let mut t = Writer::new(); 
+
+    // use core::fmt::Write; 
+    // for _ in 0..10 {
+    //     write!(t, "Hello, I'm {}! \n", "Cutie Deng").unwrap(); 
+    // }
+
+    // write!(vga_buffer::WRITER.lock(), "Hello World!\n").unwrap(); 
+    println!("Hello World!"); 
+
+    panic!("Some panic Message. "); 
+    
+    loop {} 
 }
