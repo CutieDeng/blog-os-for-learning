b58c4e7f217c3ad3864a9371e3d31a3ca14cd1eb
Cutie Deng <Dengzr2020.mail.sustech.edu.cn>
2022-06-08 01:24:29 +0800

    Add the lib module and test modules.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main] 
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 // use core::panic::PanicInfo; 
@@ -9,19 +9,15 @@
 fn panic(info: &core::panic::PanicInfo) -> ! {
     // todo!();  
     if cfg!(test) {
-        serial_println!("[failed]\n");
-        serial_println!("Error: {}\n", info);
-        exit_qemu(QemuExitCode::Failed);
+        blog_os::test_panic_handler(info); 
     } else {
         println!("{}", info); 
     }
     loop {} 
 }
 
-mod serial; 
-mod vga_buffer; 
-
-// use crate::vga_buffer::Writer; 
+use blog_os::println; 
+use blog_os::serial_println; 
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -31,29 +27,4 @@ pub extern "C" fn _start() -> ! {
     test_main(); 
 
     loop {} 
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test(); 
-    }
-    exit_qemu(QemuExitCode::Success); 
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
 }
